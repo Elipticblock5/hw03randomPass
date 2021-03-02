80da25afea5f18f62b2cd7f76c9a47fe6642e24f
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-03-01 18:20:26 -0700

    setting up variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,21 @@
-// setting up variables for 4 choice input, using in class solutions as guides.
+// setting up variables for 4 choice input, using in class solutions as guides.  Length to be done via prompt
+
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = ["!", "#", "$", "&", "%", "'", "(", ")", "*", "+", "-", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "~", "`", "{", "|", "}", "."];
+
+// The five things to confirm via prompt
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
 
-var isNumber; //user choice numbers
-var isSpecialChar; //user choice special characters
-var isUpperCase; //user choice upper case
-var isLowercase; //user choice lower case
-var passLength; //input for password length
-var userChoices; // this is variable to gather up the other user input choices 
 
-// making array strings, 4 user inputs specials, numbers, lower, upper
 
-specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];   
-upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];  
-      
-// Need to confirm string length before password generation adding submit variable to check
-//Using window alert to get length input and verify
- window.alert("Please enter a length between 8 and 128 for your password?");
 
- 
 
 
     
