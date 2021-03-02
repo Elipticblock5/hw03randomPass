100902c659c651778f067b2cea8f8a9422e52fe6
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-03-01 21:24:57 -0700

    added variable strings

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+  
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+   // Create strings  for the four user inputs to be used in random generator
+   var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYW"
+   var specialCase = "!@#$%^&*(){}[]<>=-_+/?~|"
+   var number = "0123456789"
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
