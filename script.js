e20f030d81e797ff6b18dc759ad473f3305ef1ea
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-03-01 21:31:47 -0700

    added user prompts, nothing working

--- script.js
+++ script.js
@@ -11,12 +11,30 @@ function writePassword() {
    var number = "0123456789"
 
 
+
+
+
+       // lenght of passwork prompt for user
+       var passLength = prompt("How many regular and special characters would you like your password to be. Please choose between 8 and 128");
+
+       // Ask if user want to include the following 4 criteria
+    var lowerCaseYes = window.confirm("Click OK to confirm including lowercase characters.")
+
+    var upperCaseYes = window.confirm("Click OK to confirm including uppercase characters.")
+
+    var numericYes = window.confirm("Click OK to confirm including numeric characters.")
+
+    var specialCaseYes = window.confirm("Click OK to confirm including special characters.")
+
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
